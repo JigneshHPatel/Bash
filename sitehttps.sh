9c0f2b29a1eb633330792e1449d66f255b88af4e
Jignesh Patel <36479642+JigneshHPatel@users.noreply.github.com>
2018-07-10 13:17:59 +0100

    Update sitehttps.sh

--- sitehttps.sh
+++ sitehttps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-curl clj-michelin-a1.int.mol.dmgt.net:3040/newssitemap.xml -H ‘X-FORWARDED-PROTO: https’ | grep -oe ‘http[^ ]*’ | sort
+curl /newssitemap.xml -H ‘X-FORWARDED-PROTO: https’ | grep -oe ‘http[^ ]*’ | sort
 
 
