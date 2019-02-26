c5e89ef4b862e08744777d12d312312773c95751
Jignesh Patel <30663762+jigpatel23@users.noreply.github.com>
2019-02-26 11:21:00 +0000

    Update feedWarmupA.sh

--- feedWarmupA.sh
+++ feedWarmupA.sh
@@ -3,7 +3,7 @@
 echo "status  time  size   url";
 for ((a=1;a<=2;a++)); do
 for i in $(cat feedurls.txt); do
-curl -sL -w "%{http_code} %{time_total} %{size_download} %{url_effective}\\n" -o /dev/null  "http://clj-feeds-a"$a".hsk.mol.dmgt.net:3000"$i;
+curl -sL -w "%{http_code} %{time_total} %{size_download} %{url_effective}\\n" -o /dev/null  "test$a"port"$i;
  done;
 done
 
